cabfbd485bda5bdf38ffcbb67738ee01509090a4
HieuSiis <hieudevpro1@gmail.com>
2025-05-20 08:58:10 +0700

    update path video in Charity page

--- src/components/pages/CharityPage/VideoComponent/index.tsx
+++ src/components/pages/CharityPage/VideoComponent/index.tsx
@@ -5,7 +5,10 @@ const VideoComponent = () => {
     <section className='main-container mt-[60px] flex items-center justify-center sm:mt-0 '>
       <div className='absolute -z-10 h-[1074px] w-full translate-y-[530px] bg-slate-100' />
       <video className=' rounded-xl sm:rounded-[30px]' controls>
-        <source src='/videos/charity.mp4' type='video/mp4' />
+        <source
+          src='https://ik.imagekit.io/hieunt/charity.mp4?tr=orig&updatedAt=1747705978493'
+          type='video/mp4'
+        />
         <track src='captions.vtt' kind='captions' label='English captions' />
       </video>
     </section>
